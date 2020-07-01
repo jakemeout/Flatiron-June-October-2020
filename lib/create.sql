e48cb6778c38a3d988f1e9c1db6a0f8337b1646b
jakemeout <jacobosity@gmail.com>
2020-07-01 18:34:48 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date INT, end_date INT);
+
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
+ 
